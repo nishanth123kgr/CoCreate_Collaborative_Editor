cf4c22a54344b7559b3a4c981f03d0467965362f
Nishanth R <nishanth123kgr@gmail.com>
2023-08-14 23:35:00 +0530

    Changes

--- public/js/domHandler.js
+++ public/js/domHandler.js
@@ -1,39 +1,18 @@
-import { generateIDS } from "./generateIDS";
+import { findTopParent, getIndexFromDoc, getJSON } from "./utils";
 import * as Automerge from "@automerge/automerge";
 import { editor } from "./initializeEditor";
+import { generateIDS } from "./generateIDS";
 
 let doc = Automerge.init()
 
-function getJSON(node) {
-    generateIDS(node);
-    return domJSON.toJSON(node, {
-        attributes: true,
-        domProperties: {
-            values: ['nodeName', 'nodeType', 'textContent', 'id']
-        },
-        metadata: false
-    });
-}
+
 
 function updateDoc(newdoc) {
     doc = newdoc;
 }
 
-function findTopParent(node) {
-    if (node) {
-        if (node.parentNode.nodeName === 'BODY')
-            return node;
-        else
-            return findTopParent(node.parentNode);
-    }
-}
-
-function getIndexFromDoc(node, doc) {
-    return doc.body.indexOf(doc.body.find((element) => element.id === node.id))
-}
-
 export function addHandler(node) {
-    let addedNode = getJSON(node)
+    generateIDS(node);
     if (node.tagName !== 'SPAN' && node.id.slice(0, -2) !== 'mceResizeHandle') {
         let newdoc = Automerge.change(doc, (doc) => {
             if (!doc.body) doc.body = []
@@ -42,8 +21,11 @@ export function addHandler(node) {
             console.log(node)
             if (node.parentNode.nodeName === 'BODY') {
                 let topParent = findTopParent(node)
+                console.log(topParent.previousSibling);
                 if (topParent.previousSibling) {
+                    let addedNode = getJSON(topParent)
                     let nodeIndex = getIndexFromDoc(topParent.previousSibling, doc) + 1
+                    if(getIndexFromDoc(topParent, doc) === -1)
                     doc.body.insertAt(nodeIndex, addedNode)
                 } else {
                     doc.body.push(addedNode)
@@ -56,7 +38,9 @@ export function addHandler(node) {
 }
 
 export function removeHandler(node) {
-    if (node.tagName !== 'BR' && node.tagName !== 'SPAN' && node.id.slice(0, -2) !== 'mceResizeHandle') {
+    generateIDS(node);
+    if(node)
+    if (node.tagName !== 'BR' || node.tagName !== 'SPAN' || node.id.slice(0, -2) !== 'mceResizeHandle') {
         let delIndex = getIndexFromDoc(node, doc);
         let newdoc = Automerge.change(doc, (doc) => {
             if (!doc.body) doc.body = []
@@ -70,11 +54,15 @@ export function removeHandler(node) {
 
 export function updateHandler(node) {
     if (node)
-        if (node.tagName !== 'BR' && node.tagName !== 'SPAN' && node.id.slice(0, -2) !== 'mceResizeHandle') {
+        if (node.tagName !== 'BR' || node.tagName !== 'SPAN' || node.id.slice(0, -2) !== 'mceResizeHandle') {
             let newdoc = Automerge.change(doc, (doc) => {
                 if (!doc.body) doc.body = []
-                let nodeIndex = getIndexFromDoc(node, doc)
-                doc.body[nodeIndex] = getJSON(node)
+                if (node.parentNode) {
+                    node = findTopParent(node)
+                    console.log(node);
+                    let nodeIndex = getIndexFromDoc(node, doc)
+                    doc.body[nodeIndex] = getJSON(node)
+                }
             })
             updateDoc(newdoc);
             console.log(doc.body);
---
+++ public/js/utils.js
@@ -0,0 +1,26 @@
+export function findTopParent(node) {
+    if (node) {
+        if (node.parentNode.nodeName === 'BODY')
+            return node;
+        else
+            return findTopParent(node.parentNode);
+    }
+}
+
+export function getIndexFromDoc(node, doc) {
+    try {
+        return doc.body.indexOf(doc.body.find((element) => element.id === node.id))
+    } catch (error) {
+        return -1
+    }
+}
+
+export function getJSON(node) {
+    return domJSON.toJSON(node, {
+        attributes: true,
+        domProperties: {
+            values: ['nodeName', 'nodeType', 'textContent', 'id']
+        },
+        metadata: false
+    });
+}
