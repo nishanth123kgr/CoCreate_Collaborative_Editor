890384d9805878306a9391095746f4ae78c632b0
Nishanth R <nishanth123kgr@gmail.com>
2023-08-25 07:27:17 +0530

    fb

--- server.js
+++ server.js
@@ -5,8 +5,20 @@ const http = require('http');
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 const { v4: uuidv4 } = require('uuid');
+var admin = require("firebase-admin");
+const Y = require('yjs');
+let ydoc = new Y.Doc();
 
+var serviceAccount = require("./cocreate-e498b-firebase-adminsdk-q96du-d87265ed48.json");
 
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://cocreate-e498b-default-rtdb.firebaseio.com"
+});
+
+var db = admin.database();
+
+// Middleware
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
@@ -16,12 +28,34 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/editor', (req, res) => {  
+app.get('/editor', (req, res) => {
     let uid = uuidv4();
+    const yjsSnapshot = Y.encodeStateAsUpdate(ydoc);
+    const serializedSnapshot = JSON.stringify(yjsSnapshot);
+    db.ref('documents/' + uid).set(serializedSnapshot).then(() => {
+        console.log('Document saved!');
+    }).catch((error) => {
+        console.log('Document failed to save: ', error);
+    });
     res.redirect(`/editor/${uid}`);
 });
 
 app.get('/editor/:id', (req, res) => {
+    let id = req.params.id;
+    db.ref('documents/' + id).once('value').then((snapshot) => {
+        const serializedSnapshot = snapshot.val();
+        if (serializedSnapshot) {
+          const yjsSnapshot = JSON.parse(serializedSnapshot);
+          Y.applyUpdate(ydoc, yjsSnapshot);
+          console.log('Yjs document loaded from Firebase');
+          console.log(ydoc.toJSON());
+        }
+      })
+      .catch((error) => {
+        console.error('Error loading Yjs document:', error);
+      });
+    
+
     res.render('editor');
 });
 
